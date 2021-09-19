857879379e7437e024e789faaf78b6e3d544619b
mohammad-alavi <mohammad.alavi1990@gmail.com>
2021-09-19 18:30:57 +0430

    remove useless web files

--- UI/WEB/Routes/ProviderCallback.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Containers\Vendor\SocialAuth\UI\WEB\Controllers\Controller;
-use Illuminate\Support\Facades\Route;
-
-Route::post('auth/{provider}/callback', [Controller::class, 'handleCallbackAll'])
-    ->name('web_socialAuth_callback');
